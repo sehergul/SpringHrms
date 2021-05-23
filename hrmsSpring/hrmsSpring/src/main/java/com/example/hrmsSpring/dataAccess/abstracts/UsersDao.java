239e9d9e451ec2413cd23d9e5fb60c658dcfb937
Seher Nur GÜL <sehernege@gmail.com>
2021-05-23 13:41:56 +0300

    Update UsersDao.java

--- hrmsSpring/hrmsSpring/src/main/java/com/example/hrmsSpring/dataAccess/abstracts/UsersDao.java
+++ hrmsSpring/hrmsSpring/src/main/java/com/example/hrmsSpring/dataAccess/abstracts/UsersDao.java
@@ -6,4 +6,8 @@ import com.example.hrmsSpring.entities.concretes.Users;
 
 public interface UsersDao extends JpaRepository<Users, Integer>{
 
+//interface, interface'i extend eder
+//JpaRepository<'Entity' annotation ile suslenmis nesne, primary veri turu>
+//JpaRepository<Hangi tablo için çalışacak, oradaki primary veri türü nedir> 
+  
 }
